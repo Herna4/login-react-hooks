e3c72ea2ef4e34229b3f8c29d8ab58970c6d5bb4
Herna Michel <labellehernamichel@yahoo.fr>
2020-10-22 16:52:24 -0400

    Build Express Server Skeleton

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const API_PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+//add route
+app.get("/:name", (req, res) => {
+  res.send("Hello, " + req.params.name);
+});
+
+app.listen(API_PORT, () => console.log(`Listening on Port ${API_PORT}`));
